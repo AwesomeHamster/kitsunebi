f1e9ecc8e63a7b94f4b4e7a344ce02e324c3b1f7
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2021-05-31 11:46:11 +0000

    feat: add lick command

---
+++ src/commands/lick.ts
@@ -0,0 +1,25 @@
+import Axios from "axios";
+import { CommandModule } from "../model/command";
+
+type Chp = string;
+
+const API_URL = "https://chp.shadiao.app/api.php";
+
+const getChp = async (): Promise<Chp> => {
+  const resp = await Axios.get(API_URL, {
+    responseType: "text",
+  });
+  const text = resp.data as Chp;
+  return text;
+}
+
+export const command = ["lick", "舔", "跪舔", "lap"];
+export const description = "回复随机彩虹屁";
+export const action: CommandModule["action"] = async (action) => {
+  try {
+    const chp = await getChp();
+    action.reply(chp);
+  } catch (error) {
+    action.reply("服务出错，请稍后重试");
+  }
+};
